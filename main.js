c4ee1f8da1be6c64949ee8ad55d92e584dd6e14a
John Kinghorn <john.kinghorn@coxautoinc.com>
2019-03-28 16:38:58 -0400

    update

---
+++ main.js
@@ -0,0 +1,66 @@
+const Files = require('./lib/data.js')
+const {app, BrowserWindow, ipcMain } = require('electron')
+const clipboard = require('electron-clipboard-extended')
+
+let mainWindow;
+let data;
+
+function createWindow ( data ) {
+  mainWindow = new BrowserWindow({
+    width: 250,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    },
+    applicationData: data
+  })
+
+mainWindow.loadFile('index.html')
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+ipcMain.on( 'save-data',( event,args ) => {
+    Files.CreateFile( 'data/data.json',args,() => {
+        console.log("FILE SUCCESSFULLY SAVED!")
+        Files.CheckForFile( 'data',( file ) => {
+            data = file;
+            mainWindow.webContents.send( 'loaded-data',file )
+        })
+    })
+})
+
+ipcMain.on( 'apply-clipboard',(event,args) => {
+    clipboard.writeText( args )
+})
+
+app.on('ready', () => {
+    Files.CheckForFile( 'data',( file ) => {
+        createWindow( file );
+        data = file;
+        mainWindow.webContents.on( 'did-finish-load',() => {
+            mainWindow.webContents.send( 'loaded-data',file )
+        })
+    })
+})
+
+app.on('window-all-closed', function () {
+    app.quit()
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
+
+//Logic for jumpcut type features.
+clipboard.on('text-changed', () => {
+    if ( data !== undefined ) {
+        data.clipboard.push( clipboard.readText() )
+        Files.CreateFile( 'data/data.json',data,() => {
+            mainWindow.webContents.send( 'loaded-data',data )
+        })
+    }
+}).startWatching()
---
+++ renderer.js
@@ -0,0 +1,105 @@
+const { ipcRenderer } = require( 'electron' )
+const UiBuilder = require('./lib/ui-builder.js')
+const Toast = require('./lib/toast.js')
+
+let data;
+let screen = "notes";
+
+
+
+ipcRenderer.on( 'loaded-data',( event,arg ) => {
+    //Once the data has been loaded we can fill out the data however we want in the ui;
+    switch ( screen ) {
+        case "notes" :
+            RenderInputs( arg )
+        break;
+        case "clipboard":
+            RenderClipBoard( arg )
+        break;
+    }
+    
+    data = arg;
+    UiBuilder.Build( data,( tar ) => {
+        screen = tar;
+    
+        switch ( screen ) {
+            case "clipboard":
+                RenderClipBoard ( data )
+            break;
+        }
+    } );
+})
+
+//Renders all the input lines based on the size of the window.
+function RenderInputs ( data ) {
+    let list = document.getElementById( 'note-list' );
+    let fields = document.getElementsByClassName( 'note-field' );
+    let amount = Math.floor(window.innerHeight / fields[0].offsetHeight);
+    let output;
+
+    if ( fields.length < amount ) {
+        output = list.innerHTML
+    } else {
+        output = ''
+    }
+
+    for ( let i = 0;i < amount;i++ ) {
+        let value = '';
+
+        if ( data.notes[i] !== undefined ) {
+            value = data.notes[i].title
+        }
+
+        output += '<li id = "note-' + i + '" class = "note-field"><input id = "note-input-' + i + '" type = "text" value="' + value + '"/></li>';
+    }
+
+    list.innerHTML = output;
+    InitInputEvents()
+}
+
+function InitInputEvents () {
+    let fields = document.getElementsByClassName( 'note-field' );
+
+    for ( let i = 0;i < fields.length;i++ ) {
+        fields[i].addEventListener('change', ( event ) => {
+            console.log("working")
+            let id = event.target.id.split('-')[2]
+
+            data.notes[i] = {
+                id: id,
+                title: event.target.value,
+                body: ""
+            }
+
+            ipcRenderer.send( 'save-data',data )
+        });
+    }
+}
+
+function RenderClipBoard ( data ) {
+    document.getElementById('clipboard-list').innerHTML = ''
+    let items = [];
+
+    for ( let i = data.clipboard.length - 1;i > 0;i-- ) {
+        item = document.createElement('LI');
+        item.classList.add('clip');
+        item.innerText = data.clipboard[i]
+        item.addEventListener('click', ( event ) => {
+            ipcRenderer.send( 'apply-clipboard',event.target.innerText )
+            Toast.ShowToast( 'Saved to Clipboard!',1500 )
+        })
+        items.push( item )
+    }
+    
+    items.forEach( item => {
+        document.getElementById('clipboard-list').appendChild( item )
+    })
+}
+
+window.onresize = function () {
+    if ( screen === "notes" ) {
+        //We need to determine how many inputs are already there and 
+        //add if it gets bigger.
+        RenderInputs( data )        
+    }
+}
